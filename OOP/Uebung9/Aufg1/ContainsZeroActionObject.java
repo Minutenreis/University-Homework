4d97e92b5fb8b85966be5b01601872ae31c31788
Minutenreis <minutenreis@gmail.com>
2022-07-08 11:50:13 +0200

    Vereinfachungen OOP Ueb9

--- OOP/Uebung9/Aufg1/ContainsZeroActionObject.java
+++ OOP/Uebung9/Aufg1/ContainsZeroActionObject.java
@@ -2,10 +2,8 @@ public class ContainsZeroActionObject implements ActionObject {
     boolean containsZero = false;
 
     public void action(Node n) {
-        if (Integer.class.isInstance(n.data)) {
-            if ((int) n.data == 0) {
-                containsZero = true;
-            }
+        if (n.data instanceof Integer && (int) n.data == 0) {
+            containsZero = true;
         }
     }
 }
--- OOP/Uebung9/Aufg1/PosSumActionObject.java
+++ OOP/Uebung9/Aufg1/PosSumActionObject.java
@@ -2,7 +2,7 @@ public class PosSumActionObject implements ActionObject{
     int sum = 0;
     
     public void action(Node n){
-        if(Integer.class.isInstance(n.data) && (int)n.data > 0){
+        if(n.data instanceof Integer && (int)n.data > 0){
             sum += (int) n.data;
         }
     }

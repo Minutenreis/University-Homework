71f0e44d184c9500450527f476ab34db91f4e6c4
Minutenreis <minutenreis@gmail.com>
2023-04-09 12:59:13 +0200

    Setup support for other students

--- Numerik/Week_1/Hausaufgabe_1_4.py
+++ Numerik/Week_1/Hausaufgabe_1_4.py
@@ -10,7 +10,7 @@ a = np.double(1.61801916)
 b = np.double(1.61801917)
 
 h = np.double(0.00000000002)
-xrange = np.arange(a, b+h, h)
+xrange = np.arange(a, b+h, h)  # np.linspace(a, b, 100) das nächste mal?
 plt.plot(xrange, f(xrange), label='$f(x)$')
 plt.legend()
 plt.grid(True)

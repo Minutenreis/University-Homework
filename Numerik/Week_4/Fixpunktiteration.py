dcc7b14b943e69874aca9465f04d451e029f7068
Minutenreis <minutenreis@gmail.com>
2023-05-01 20:15:19 +0200

    updated Num4

--- Numerik/Week_4/Fixpunktiteration.py
+++ Numerik/Week_4/Fixpunktiteration.py
@@ -19,7 +19,7 @@ def genIteration(x):
     xalt = x-1
     xrange = [x]
     i = 0
-    while (i < 20 and abs(x-xalt) > 1e-7):
+    while (i < 20 and np.abs(x-xalt) > 1e-7):
         xalt = x
         x = phi(x)
         xrange.append(x)
@@ -27,10 +27,10 @@ def genIteration(x):
     return xrange
 
 
-def genIteration1(x, xalt,):
+def genIterationApprox(x, xalt):
     xrange = [x]
     i = 0
-    while (i < 20 and abs(x-xalt) > 1e-7):
+    while (i < 20 and np.abs(x-xalt) > 1e-7):
         temp = x
         x = phi1(x, xalt)
         xalt = temp
@@ -43,41 +43,19 @@ n = 10
 
 valueRange = np.linspace(0.5, 1, 10)
 
-# x = 0.999999999999412
-# y = 0.6568433
-# z = 0.6568433
-
-# xrange = genIteration(x)
-# yrange = genIteration(y)
-# zrange = genIteration(z)
-
-# plt.plot(range(len(xrange)), xrange, label=f"f({x})")
-# plt.plot(range(len(yrange)), yrange, label=f"f({y})")
-# plt.plot(range(len(zrange)), zrange, label=f"f({z})")
-
-# for x in valueRange:
-#     xrange = genIteration(x)
-#     plt.plot(range(len(xrange)), xrange, label=f"f({x})")
-
-for n in range(10):
+for n in range(100):
     x = 0.5 + random.random()/2
     xrange = genIteration(x)
-    plt.plot(range(len(xrange)), xrange, label=f"f({x})")
+    plt.plot(range(len(xrange)), xrange, "-k", label=f"f({x})")
 
-# x = 0.999999999999412
-# y = 0.6568433
-# z = 0.823423
-# xalt = 0.6
-# yalt = 0.6
-# zalt = 0.8
 
-# xrange = genIteration1(x, xalt)
-# yrange = genIteration1(y, yalt)
-# zrange = genIteration1(z, zalt)
-
-# plt.plot(range(len(xrange)), xrange, label=f"f({x}) approx")
-# plt.plot(range(len(yrange)), yrange, label=f"f({y}) approx")
-# plt.plot(range(len(zrange)), zrange, label=f"f({z}) approx")
+for n in range(100):
+    x = 0.5 + random.random()/2
+    xalt = 0.5 + random.random()/2
+    while (x == xalt):
+        xalt = 0.5 + random.random()/2
+    xrange = genIterationApprox(x, xalt)
+    plt.plot(range(len(xrange)), xrange, "-r", label=f"f({x}) approx")
 
 plt.legend()
 plt.show()

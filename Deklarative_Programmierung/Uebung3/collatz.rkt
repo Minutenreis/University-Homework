592fdf34e71c20edd88544788ea7ef2e75e676d1
Minutenreis <minutenreis@gmail.com>
2023-05-10 23:54:24 +0200

    Dekla_3_9

--- Deklarative_Programmierung/Uebung3/collatz.rkt
+++ Deklarative_Programmierung/Uebung3/collatz.rkt
@@ -50,27 +50,21 @@
   (reverse (fast-collatz-helper n '()))
   )
 
-(define (fast-collatz-helper n list)
-  (if (member n list)
-     list
-     (cond
-       [(<= n 4) (collatz-helper n list)]; base case for 4 2 1; repetition possible so normal function
-       [(even? n) (fast-collatz-helper (/ n 2) (cons n list))]
-       [else (fast-collatz-helper (/ (+ (* 3 n) 1) 2) (cons (+ (* 3 n) 1) (cons n list)))] ; calculates 2 steps at once
-       )
-     )
+(define (fast-collatz-helper n list); remove O(n) call to member by using knowledge of collatz
+  (cond
+    [(<= n 4) (collatz-helper n list)]; base case for 4 2 1
+    [(even? n) (fast-collatz-helper (/ n 2) (cons n list))]
+    [else (fast-collatz-helper (/ (+ (* 3 n) 1) 2) (cons (+ (* 3 n) 1) (cons n list)))] ; calculates 2 steps at once
+    )
   )
 
 
 
-(displayln (collatz 67))
-(displayln (fast-collatz 67))
-(displayln (b_collatz 2))
-(displayln (b_fast_collatz 2))
+(displayln (collatz 8))
+(displayln (fast-collatz 8))
+(displayln (b_collatz 22))
+(displayln (b_fast_collatz 22))
 
 ; ich glaube es gibt weitere optimierungen, wo man die längen der vorherig ausgerechneten listen in der Berechnung neuer nutzt
 ; doch das übersteigt mir gerade, was ich hier erreichen will; also lediglich eine schnellere Variante von Collatz
-
-; Idee für andere Optimierung: Längen der jeweilig vorherigen Folgen abspeichern und wenn man in der geradigen Collatzfolge
-; in den Startwert eine der vorherigen rutscht, einfach deren (beim Berechnen abgespeicherte) Folge anhängen statt nochmal zu rechnen
 

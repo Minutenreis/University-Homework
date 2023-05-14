d126ac1455e4c7240721b09c56b76d9cc4104761
Minutenreis <minutenreis@gmail.com>
2023-05-14 21:32:58 +0200

    Numerik Newton Corrected

--- Numerik/Week_6/newton2d.py
+++ Numerik/Week_6/newton2d.py
@@ -4,19 +4,23 @@ import matplotlib.pyplot as plt
 # F(x,y,z)
 
 
-def F(x, y, z): return np.array([[x**2+10*y*z], [20*y**2*z + 3*x*z - 5]])
+def F(x, y, z): return np.array([[x**2+10*y*z], [20*y**2*z + 3*x*y - 5]])
 
 # Inverse of Jacobimatrix of f(y,z)=F(x,y,z) (x is constant)
 
 
-def Jfinv(x, y, z): return 1/(20*y**2*z+30*x*y) * \
+def Jfinv(x, y, z): return 1/(20*y**2*z+3*x*y) * \
     np.array([[-2*y**2, y], [4*y*z+3/10*x, -z]])
 
 # Newton-Funktion
 
 
-def Nf(x, y, z): return np.array(
-    [[y], [z]]) - np.dot(Jfinv(x, y, z), F(x, y, z))
+def Nf(x, y, z): return np.array([[y], [z]]) - Jfinv(x, y, z) @ F(x, y, z)
+
+
+def Nf2(x, y, z): return np.array([[y], [z]])-1/(20*y**2*z+3*x*y) * np.array(
+    [[-2*y**2*x**2+3*x*y**2-5*y], [4*x**2*y*z+20*y**2*z**2+3/10*x**3+5*z]])
+
 
 # Newton-Verfahren
 
@@ -24,7 +28,8 @@ def Nf(x, y, z): return np.array(
 def Newton(x, y, z, maxit):
     for i in range(maxit):
         [y], [z] = Nf(x, y, z)
-        if np.abs(F(x, y, z)[0][0]) < 10**(-8) and np.abs(F(x, y, z)[1][0]) < 10**(-8):
+        [F1], [F2] = F(x, y, z)
+        if np.abs(F1) < 10**(-8) and np.abs(F2) < 10**(-8):
             return y, z
     print(x)  # find if maxit is sufficient
     return y, z
@@ -32,10 +37,15 @@ def Newton(x, y, z, maxit):
 
 xrange = np.linspace(-3, -0.5, 1000)
 
-for x in xrange:
-    y, z = Newton(x, 1, 1, 1000)
-    plt.scatter(y, z, s=9, c="r", marker=".")
+yValues = []
+zValues = []
 
+for x in xrange:
+    y, z = Newton(x, 0.2, 1, 1000)
+    yValues.append(y)
+    zValues.append(z)
+
+plt.plot(yValues, zValues)
 plt.ylabel("z")
 plt.xlabel("y")
 plt.show()

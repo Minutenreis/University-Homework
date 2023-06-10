e536b4707387c91f24f034674bba5656087e9a22
Minutenreis <minutenreis@gmail.com>
2023-06-10 13:24:17 +0200

    Numerik Korrektur Thomas Algo

--- Numerik/Week_10/vollstaendiges_kubisches_Spline.py
+++ Numerik/Week_10/vollstaendiges_kubisches_Spline.py
@@ -3,10 +3,14 @@ import matplotlib.pyplot as plt
 
 
 def f(tx):
-    return 1 if tx == 0 else 0
+    return np.cos(np.pi/2 * tx)
 
 
-t = [i-8 for i in range(17)]
+def df(tx):
+    return -np.pi/2 * np.sin(np.pi/2 * tx)
+
+
+t = [-2, -1, 0, 1, 2]
 
 
 # https://en.wikipedia.org/wiki/Tridiagonal_matrix_algorithm
@@ -29,26 +33,28 @@ def TDMA(a, b, c, d):
     return x
 
 
-h = [t[i+1]-t[i] for i in range(len(t)-1)]
-b = [2 for i in range(len(t)-2)]
-lambda1 = [h[i]/(h[i-1]+h[i]) for i in range(1, len(t)-1)]  # c
-mu = [h[i-1]/(h[i-1]+h[i]) for i in range(1, len(t)-1)]  # a
+n = len(t)-1  # array starts at 0
+h = [t[i+1]-t[i] for i in range(n)]
+b = [2 for i in range(n+1)]
+lambda1 = [1, *[h[i]/(h[i-1]+h[i]) for i in range(1, n)], 1]  # c
+mu = [1, *[h[i-1]/(h[i-1]+h[i]) for i in range(1, n)], 1]  # a
 coeff2 = [(f(t[i])-f(t[i-1]))/(t[i]-t[i-1])
-          for i in range(1, len(t))]  # f[t_i-1, t_i]
+          for i in range(1, n+1)]  # f[t_i-1, t_i]
 coeff3 = [(coeff2[i]-coeff2[i-1])/(h[i-1]+h[i])
           for i in range(1, len(coeff2))]  # f[t_i-1, t_i, t_i+1]
-d = [6*coeff3[i] for i in range(len(coeff3))]  # d
+d = [6*(coeff2[0]-df(t[0]))/h[0],
+     *[6*coeff3[i] for i in range(len(coeff3))],
+     6*(df(t[n])-coeff2[n-1])/h[n-1]]  # d
 
-M = [0, *TDMA(mu, b, lambda1, d), 0]
-print(M)
+M = TDMA(mu, b, lambda1, d)
 C = [(f(t[i+1])+f(t[i]))/2 - h[i]**2/12 * (M[i+1]+M[i])
-     for i in range(len(t)-1)]
+     for i in range(n)]
 D = [(f(t[i+1])-f(t[i]))/h[i] - h[i]/6 * (M[i+1]-M[i])
-     for i in range(len(t)-1)]
+     for i in range(n)]
 
 
 def spline(tx):
-    for i in range(len(t)-1):
+    for i in range(n):
         if t[i] <= tx and tx <= t[i+1]:
             return splineHelp(tx, i)
     print(tx)
@@ -59,14 +65,18 @@ def splineHelp(tx, i):
 
 
 xRange = np.linspace(t[0], t[-1], 1000)
-print("t =", t)
-print("h =", h)
-print("C =", C)
-print("D =", D)
-print("M =", M)
-print("f =", [f(tx) for tx in t])
+print("mu =", [round(tx, 3) for tx in mu])
+print("lambda1 =", [round(tx, 3) for tx in lambda1])
+print("b =", [round(tx, 3) for tx in b])
+print("d = ", [round(tx, 3) for tx in d])
+print("t =", [round(tx, 3) for tx in t])
+print("h =", [round(tx, 3) for tx in h])
+print("C =", [round(tx, 3) for tx in C])
+print("D =", [round(tx, 3) for tx in D])
+print("M =", [round(tx, 3) for tx in M])
+print("f =", [round(f(tx), 3) for tx in t])
 print("spline =", [round(spline(tx), 3) for tx in t])
-plt.plot(t, [f(x) for x in t], ".", label="f")
+plt.plot(xRange, [f(x) for x in xRange], label="f")
 plt.plot(xRange, [spline(x) for x in xRange], label="spline")
 plt.legend()
 plt.show()

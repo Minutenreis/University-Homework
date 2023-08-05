dc717804de8268f4b2c860dd9817f715533af760
Minutenreis <minutenreis@gmail.com>
2023-08-05 06:49:29 +0200

    Updated Lolalytics Scripts

--- scripts/Lolalytics_WR_Normalizer.js
+++ scripts/Lolalytics_WR_Normalizer.js
@@ -4,31 +4,43 @@
 // @run-at document-idle
 // ==/UserScript==
 
-setTimeout(main, 250)
+var fireOnHashChangesToo = true;
+var pageURLCheckTimer = setInterval(
+    function () {
+        if (this.lastPathStr !== location.pathname
+            || this.lastQueryStr !== location.search
+            || (fireOnHashChangesToo && this.lastHashStr !== location.hash)
+        ) {
+            this.lastPathStr = location.pathname;
+            this.lastQueryStr = location.search;
+            this.lastHashStr = location.hash;
+            setTimeout(main, 250);
+        }
+    }
+    , 111
+);
 
 function main() {
-    marker = "_"
-    avgWRText = document.getElementsByClassName("Analysed_wrapper__o86fv")[0].innerText;
-    if (!avgWRText.startsWith(marker)) {
-        avgWRArr = avgWRText.split("\n")[0].split(" ")
-        avgWR = avgWRArr[avgWRArr.length - 1].slice(0, -2);
-        console.log(avgWR)
-        //mark WR for site selection
-        document.getElementsByClassName("Analysed_wrapper__o86fv")[0].innerHTML = marker + document.getElementsByClassName("Analysed_wrapper__o86fv")[0].innerHTML
 
-        tableData = document.getElementsByClassName("TierList_list__j33gd")[0].children
-        for (var div of tableData) {
-            index = div.childNodes[0].className.startsWith("ListRow_rank") ? 5 : 4;
-            WR = Math.round((div.childNodes[index].innerText.split("\n")[0] - avgWR + 50) * 100) / 100
-            ogStat = div.childNodes[index].innerText.split("\n")
-            div.childNodes[index].innerHTML = '<div> ' + WR + '<\div><div class="ListRow_wrdelta__dKTY+">' + ogStat[0] + "<br />" + ogStat[1] + "</div>"
-            if (WR > 50.5) {
-                div.childNodes[index].style["color"] = "green"
-            } else if (WR > 49.5) {
-                div.childNodes[index].style["color"] = "yellow"
-            } else {
-                div.childNodes[index].style["color"] = "red"
-            }
+    //get average Winrate of Elo Bracket
+    avgWRText = document.getElementsByClassName("Analysed_wrapper__o86fv")[0].innerText;
+    avgWRArr = avgWRText.split("\n")[0].split(" ")
+    avgWR = avgWRArr[avgWRArr.length - 1].slice(0, -2);
+
+    //normalize Champion WR
+    tableData = document.getElementsByClassName("TierList_list__j33gd")[0].children
+    for (var div of tableData) {
+        index = div.childNodes[0].className.startsWith("ListRow_rank") ? 5 : 4;
+        WR = Math.round((div.childNodes[index].innerText.split("\n")[0] - avgWR + 50) * 100) / 100
+        ogStat = div.childNodes[index].innerText.split("\n")
+        div.childNodes[index].innerHTML = '<div> ' + WR + '<\div><div class="ListRow_wrdelta__dKTY+">' + ogStat[0] + "<br />" + ogStat[1] + "</div>"
+        //coloring
+        if (WR > 50.5) {
+            div.childNodes[index].style["color"] = "green"
+        } else if (WR > 49.5) {
+            div.childNodes[index].style["color"] = "yellow"
+        } else {
+            div.childNodes[index].style["color"] = "red"
         }
     }
 }
--- scripts/Lolalytics_WR_Normalizer_Champ.js
+++ scripts/Lolalytics_WR_Normalizer_Champ.js
@@ -4,23 +4,38 @@
 // @run-at document-idle
 // ==/UserScript==
 
-setTimeout(main, 250)
-console.log("test")
+var fireOnHashChangesToo = true;
+var pageURLCheckTimer = setInterval(
+    function () {
+        if (this.lastPathStr !== location.pathname
+            || this.lastQueryStr !== location.search
+            || (fireOnHashChangesToo && this.lastHashStr !== location.hash)
+        ) {
+            this.lastPathStr = location.pathname;
+            this.lastQueryStr = location.search;
+            this.lastHashStr = location.hash;
+            setTimeout(main, 250);
+        }
+    }
+    , 111
+);
 
 function main() {
-    marker = "_"
+    console.log("test")
+
     avgWRText = document.getElementsByClassName("Analysed_wrapper__o86fv")[0].innerText;
-    if (!avgWRText.startsWith(marker)) {
-        avgWRArr = avgWRText.split("\n")[0].split(" ")
-        avgWR = avgWRArr[avgWRArr.length - 1].slice(0, -2);
-        //mark WR for site selection
-        document.getElementsByClassName("Analysed_wrapper__o86fv")[0].innerHTML = marker + document.getElementsByClassName("Analysed_wrapper__o86fv")[0].innerHTML
+    avgWRArr = avgWRText.split("\n")[0].split(" ")
+    avgWR = avgWRArr[avgWRArr.length - 1].slice(0, -2);
 
-
-        StatContainer = document.getElementsByClassName("ChampionStats_stats__26e3l")[0].children
+    //normalize WR of champ
+    StatContainer = document.getElementsByClassName("ChampionStats_stats__26e3l")[0].children
+    if (!StatContainer[0].marked) {
+        StatContainer[0].marked = true
         ogWR = StatContainer[0].childNodes[0].innerText.slice(0, -1)
+        //shifted Normalization (ogWR - (avgWR - 50))
         WR = Math.round((ogWR - avgWR + 50) * 100) / 100
         StatContainer[0].childNodes[0].innerText = WR
+        //coloring
         color = "grey"
         if (WR > 50.5) {
             color = "green"
@@ -29,7 +44,6 @@ function main() {
         } else {
             color = "red"
         }
-        console.log(WR)
         StatContainer[0].childNodes[0].innerHTML = '<div style="color:' + color + '"> ' + WR + '<\div><div style="font-size: 8px; color: grey">' + ogWR + "</div>"
     }
 }

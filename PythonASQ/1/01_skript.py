3c20a8dfd943576d6dc10ee33a275ea3fd0279a0
Minutenreis <Minutenreis@gmail.com>
2024-04-10 03:08:48 +0200

    Python Übung 1

---
+++ PythonASQ/1/01_skript.py
@@ -0,0 +1,3 @@
+print('Hallo! Ich bin (wahrscheinlich) das erste Pythonskript welches du direkt auf der Kommandozeile ausgeführt hast!')    #Und ich bin ein Kommentar, ich kann direkt hinter Pythoncode stehen.
+
+# Ich bin auch ein Kommentar, ich kann auch auf einer eigenen Zeile stehen.

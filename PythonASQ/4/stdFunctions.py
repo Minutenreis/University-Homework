25aeb59644366e9fc41f4d8f30071400115a9acf
Minutenreis <Minutenreis@gmail.com>
2024-05-02 14:16:21 +0200

    Adjusted range algorithm

--- PythonASQ/4/stdFunctions.py
+++ PythonASQ/4/stdFunctions.py
@@ -68,6 +68,30 @@ def range_control(start:int, stop:int = None, step:int = 1) -> list[int]:
         start += step
     return numbers
 
+class RangeIterator:
+    curr = 0;
+    stop = 0;
+    step = 1;
+    
+    def __init__(self, start:int, stop:int = None, step:int = 1) -> list[int]:
+        """Returns a list of numbers from start to stop with the given step."""
+        if step == 0:
+            raise ValueError("range() arg 3 must not be zero")
+        if stop == None:
+            self.stop = start
+            self.curr = 0
+        else:
+            self.curr = start
+            self.stop = stop
+        self.step = step
+    
+    def __next__(self):
+        if self.curr >= self.stop and self.step > 0 or self.curr <= self.stop and self.step < 0:
+            raise StopIteration
+        val = self.curr
+        self.curr += self.step
+        return val
+
 class Range:
     curr = 0;
     stop = 0;
@@ -86,14 +110,7 @@ class Range:
         self.step = step
     
     def __iter__(self):
-        return self
-    
-    def __next__(self):
-        if self.curr >= self.stop and self.step > 0 or self.curr <= self.stop and self.step < 0:
-            raise StopIteration
-        val = self.curr
-        self.curr += self.step
-        return val
+        return RangeIterator(self.curr, self.stop, self.step)
     
     def __repr__(self):
         return "range(" + str(self.start) + ", " + str(self.stop) + ", " + str(self.step) + ")"
@@ -101,8 +118,10 @@ class Range:
 def range(start:int, stop:int = None, step:int = 1) -> Range:
     return Range(start, stop, step)
 
-x = range(10, 3, -4)
+x = range(10, 3, -2)
 for i in x:
     print(i)
-
+print()
+for i in x:
+    print(i)
     

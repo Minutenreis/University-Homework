8b34e83a3db9339979d3a56a1003b43f57a76163
Minutenreis <Minutenreis@gmail.com>
2024-06-04 11:53:58 +0200

    Python ASQ 9.2

--- PythonASQ/9/geld.py
+++ PythonASQ/9/geld.py
@@ -1,5 +1,6 @@
 class Geld:
-    __exchange_rates_eur = {
+    __basis = 'EUR'
+    __exchange_rates = {
     'EUR': 1,
     'USD': 1.09,
     'GBP': 0.85,
@@ -7,39 +8,60 @@ class Geld:
     }
     
     @classmethod
-    def exchange_rates_eur(cls):
-        return cls.__exchange_rates_eur
+    def basis(cls):
+        return cls.__basis
     
     @classmethod
-    def add_exchange_rates_eur(cls, rate: tuple[str, float]):
-        if rate[0] in cls.__exchange_rates_eur:
+    def setNewBasis(cls, new_basis):
+        if new_basis not in cls.__exchange_rates:
+            raise ValueError('Unbekannte Währung')
+        exchange_rate_basis = cls.__exchange_rates[new_basis]
+        for key in cls.__exchange_rates:
+            cls.__exchange_rates[key] = cls.__exchange_rates[key] / exchange_rate_basis
+        cls.__basis = new_basis
+    
+    @classmethod
+    def exchange_rates(cls):
+        return cls.__exchange_rates
+    
+    @classmethod
+    def add_exchange_rates(cls, rate: tuple[str, float]):
+        if rate[0] in cls.__exchange_rates:
             raise ValueError('Wechselkurs bereits vorhanden')
-        cls.__exchange_rates_eur[rate[0]] = rate[1]
+        cls.__exchange_rates[rate[0]] = rate[1]
+    
+    @property
+    def base_currency(self):
+        return Geld.basis()
+    
+    @base_currency.setter
+    def base_currency(self, new_basis):
+        Geld.setNewBasis(new_basis)
     
     def __init__(self, value = 0, currency = 'EUR'):
-        if currency not in Geld.exchange_rates_eur():
+        if currency not in Geld.exchange_rates():
             raise ValueError('Unbekannte Währung')
-        self.__value = value
+        self.value = value
         self.__currency = currency
         
     def change_currency(self, new_currency):
-        if new_currency not in Geld.exchange_rates_eur():
+        if new_currency not in Geld.exchange_rates():
             raise ValueError('Unbekannte Währung')
-        self.__value = self.__value * Geld.exchange_rates_eur[self.__currency] / Geld.exchange_rates_eur[new_currency]
+        self.__value = self.value / Geld.exchange_rates()[self.__currency] * Geld.exchange_rates()[new_currency]
         self.__currency = new_currency
     
     def add(self, other):
         if type(other) != Geld:
             raise TypeError('Nur Geld kann zu Geld addiert werden')
-        self.__value += other.valueInEuro() / Geld.exchange_rates_eur[self.__currency]
+        self.__value += other.valueInEuro() * Geld.exchange_rates()[self.__currency]
     
     def sub(self, other):
         if type(other) != Geld:
             raise TypeError('Nur Geld kann von Geld subtrahiert werden')
-        self.__value -= other.valueInEuro() / Geld.exchange_rates_eur[self.__currency]
+        self.value -= other.valueInEuro() * Geld.exchange_rates()[self.__currency]
     
     def valueInEuro(self):
-        return self.__value * Geld.exchange_rates_eur[self.__currency]
+        return self.__value / Geld.exchange_rates()[self.__currency]
     
     @property
     def value(self):
@@ -47,6 +69,8 @@ class Geld:
     
     @value.setter
     def value(self, value):
+        if value < 0:
+            raise ValueError('Wert darf nicht negativ sein')
         self.__value = value
     
     def __str__(self):
@@ -61,10 +85,15 @@ class Geld:
     def __add__(self, other):
         if type(other) != Geld:
             raise TypeError('Nur Geld kann zu Geld addiert werden')
-        return Geld(self.__value + other.valueInEuro() / Geld.exchange_rates_eur[self.__currency], self.__currency)
+        return Geld(self.__value + other.valueInEuro() * Geld.exchange_rates()[self.__currency], self.__currency)
+    
+    def __sub__(self, other):
+        if type(other) != Geld:
+            raise TypeError('Nur Geld kann von Geld subtrahiert werden')
+        return Geld(self.__value - other.valueInEuro() * Geld.exchange_rates()[self.__currency], self.__currency)
 
 # Test
-g1 = Geld(100, 'USD')
-print(g1)
-g1.value = 200
-print(g1)
+g1 = Geld(100, 'EUR')
+g1.base_currency = 'USD'
+g2 = Geld(100, 'USD')
+print(g1 - g2)

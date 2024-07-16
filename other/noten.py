546ca6ec331903aa2a662dcc766a77afe3d9e262
Minutenreis <Minutenreis@gmail.com>
2024-07-16 13:42:52 +0200

    Adjusted marks to order in friedolin

--- other/noten.py
+++ other/noten.py
@@ -17,10 +17,6 @@ noten = [
     (1.0,6), # Diskrete Strukturen II 
     (1.0,6), # Einführung in die Wahrscheinlichkeitstheorie 
     (1.3,6), # Numerische Mathematik
-    # C
-    (1.3,6), # IT-Sicherheit 
-    (2.7,6), # Algorithmisches Beweisen
-    (1.3,3), # Projektmanagement (ASQ) 
     # A
     (1.0,9), # Grundlagen informatischer Problemlösung 
     (1.0,5), # Objektorientierte Programmierung 
@@ -35,9 +31,13 @@ noten = [
     (1.3,6), # Grundlagen der Informations- und Softwaresysteme 
     (1.3,4), # Kryptologie LAB
     (1.0,6), # Kryptologie
+    (2.3,6), # Einführung in die Künstliche Intelligenz
     (1.3,6), # Projekt - Paralleles Rechnen 
     (1.0,6), # Parallel Computing I  
-    (2.3,6), # Einführung in die Künstliche Intelligenz
+    # C
+    (1.3,6), # IT-Sicherheit 
+    (2.7,6), # Algorithmisches Beweisen
+    (1.3,3), # Projektmanagement (ASQ) 
     ]
 
 (avg, ects) = calcSchnitt(noten)
